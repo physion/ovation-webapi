6108eb4fa583989e43d21f1bdc12d38d133c3161
Adam Conroy <adam.conroy@gmail.com>
2014-05-21 15:51:57 -0400

    basic project get endpoint that returns a hardcoded list of projects and requires an api-key parameter

--- src/ovation_api_webservice/handler.clj
+++ src/ovation_api_webservice/handler.clj
@@ -1,13 +1,14 @@
 (ns ovation-api-webservice.handler
   (:use compojure.core
-        ovation-api-webservice.view)
+        ovation-api-webservice.view
+        ring.middleware.params)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [ovation-api-webservice.view :as view]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (GET "/project" [] (index-project))
+  (GET "/" [] "nothing to see here, move along")
+  (GET "/project" {params :params} (index-project params))
   (route/resources "/")
   (route/not-found "Not Found"))
 
