7cf1fdbc56f6d848944a5d61bf9708ba3e9cfd75
Keith Vedananda <kvedananda@gmail.com>
2014-05-22 19:38:51 -0400

    Added mock source and protocol views

--- src/ovation_api_webservice/handler.clj
+++ src/ovation_api_webservice/handler.clj
@@ -1,15 +1,21 @@
 (ns ovation-api-webservice.handler
   (:use compojure.core
         ovation-api-webservice.view
+        ovation-api-webservice.protocol-view
+        ovation-api-webservice.source-view
         ring.middleware.params)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [ovation-api-webservice.view :as view]
+            [ovation-api-webservice.protocol-view :as protocol-view]
+            [ovation-api-webservice.source-view :as source-view]
             [ring.middleware.cors :refer [wrap-cors]]))
 
 (defroutes app-routes
   (GET "/" [] "nothing to see here, move along")
   (GET "/project" {params :params} (index-project params))
+  (GET "/protocol" {params :params} (index-protocol params))
+  (GET "/source" {params :params} (index-source params))
   (route/resources "/")
   (route/not-found "Not Found"))
 
