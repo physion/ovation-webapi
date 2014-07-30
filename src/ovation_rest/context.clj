89ebb3d657c9806487a049b76ba085dc944dc0e4
Barry Wark <barrywark@gmail.com>
2014-07-29 23:59:33 -0400

    Some comments in context

--- src/ovation_rest/context.clj
+++ src/ovation_rest/context.clj
@@ -4,10 +4,13 @@
 
 
 
-(defn make-context [api_key]
+(defn make-context
+      "Make an Ovation DataContext for the given API key"
+      [api_key]
       ;;TODO let-if OVATION_IO_HOST_URI => default https://dev.ovation.io
       (-> (us.physion.ovation.api.web.Server/make (URI. (System/getenv "OVATION_IO_HOST_URI")) api_key)
           (.getContext)))
 
 
-(def cached-context (memo/lru make-context {} :lru/threshold 3)) ;;TODO lru/threshold should be parameterized
+;;TODO lru/threshold should be parameterized
+(def cached-context (memo/lru make-context {} :lru/threshold 3))
