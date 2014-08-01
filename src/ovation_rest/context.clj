a96ec74229c57ad92eb9ac427cf0b8d844123bbd
Barry Wark <barrywark@gmail.com>
2014-08-01 10:22:35 -0400

    Use getProperty for beanstalk

--- src/ovation_rest/context.clj
+++ src/ovation_rest/context.clj
@@ -8,8 +8,8 @@
   "Make an Ovation DataContext for the given API key"
   [api_key]
 
-  (let [api_endpoint (if (System/getenv "OVATION_IO_HOST_URI")
-                       (System/getenv "OVATION_IO_HOST_URI")
+  (let [api_endpoint (if (System/getProperty "OVATION_IO_HOST_URI")
+                       (System/getProperty "OVATION_IO_HOST_URI")
                        "https://dev.ovation.io")]
 
     (-> (us.physion.ovation.api.web.Server/make (URI. api_endpoint) api_key)
