4f835359f797413df887fc6c7514600437da49a0
Adam Conroy <adam.conroy@gmail.com>
2014-09-04 02:59:23 -0400

    merged in initial compojure api stuff, still super raw

--- project.clj
+++ project.clj
@@ -1,26 +1,30 @@
 (defproject ovation-rest "0.1.0-SNAPSHOT"
-            :description "Ovation REST API"
-            :url "http://ovation.io"
+  :description "Ovation REST API"
+  :url "http://ovation.io"
 
-            :repositories [["s3-ovation-snapshot-repository" {:url "s3p://maven.ovation.io/snapshot"}]
-                           ["s3-ovation-release-repository" {:url "s3p://maven.ovation.io/release"}]]
+  :repositories [["s3-ovation-snapshot-repository" {:url "s3p://maven.ovation.io/snapshot"}]
+                 ["s3-ovation-release-repository" {:url "s3p://maven.ovation.io/release"}]]
 
-            :dependencies [[org.clojure/clojure "1.5.1"]
-                           [org.clojure/core.incubator "0.1.3"]
-                           [compojure "1.1.6"]
-                           [ring/ring-json "0.3.1"]
-                           [ring-cors "0.1.2"]
-                           [org.clojure/core.memoize "0.5.6"]
-                           [us.physion/ovation-api "3.0.0-SNAPSHOT"]
-                           [us.physion/ovation-logging "3.0.0-SNAPSHOT"]
-                           [com.newrelic.agent.java/newrelic-agent "3.9.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.incubator "0.1.3"]
+                 [compojure "1.1.6"]
+                 [ring/ring-json "0.3.1"]
+                 [ring-cors "0.1.2"]
+                 [org.clojure/core.memoize "0.5.6"]
+                 [us.physion/ovation-api "3.0.0-SNAPSHOT"]
+                 [us.physion/ovation-logging "3.0.0-SNAPSHOT"]
+                 [metosin/ring-http-response "0.4.0"]
+                 [metosin/ring-swagger "0.12.0"]
+                 [metosin/ring-swagger-ui "2.0.17"]
+                 [metosin/compojure-api "0.15.1-edn"]
+                 [com.newrelic.agent.java/newrelic-agent "3.9.0"]]
 
-            :plugins [[lein-ring "0.8.10"]
-                      [s3-wagon-private "1.1.2"]
-                      [lein-midje "3.0.0"]
-                      [lein-elastic-beanstalk "0.2.8-SNAPSHOT"]]
+  :plugins [[lein-ring "0.8.10"]
+            [s3-wagon-private "1.1.2"]
+            [lein-midje "3.0.0"]
+            [lein-elastic-beanstalk "0.2.8-SNAPSHOT"]]
 
-            :ring {:handler ovation-rest.handler/app}
+  :ring {:handler ovation-rest.handler/app}
 
             :aws {:beanstalk {:stack-name   "64bit Amazon Linux running Tomcat 7"
                               :environments [{:name "development"
@@ -35,17 +39,22 @@
                                               :env  {"OVATION_IO_HOST_URI" "https://ovation.io"
                                                      "NEWRELIC" "newrelic_license_key"}}]}}
 
-            :profiles {:dev     {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                                [ring-mock "0.1.5"]
-                                                [midje "1.6.3"]]}
+  :profiles {
+              :ovation-web-api {:ring {:handler ovation-rest.handler/app}}
+              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                   [ring-mock "0.1.5"]
+                                   [midje "1.6.3"]] }
+              :jenkins {:aws {:access-key ~(System/getenv "AWS_ACCESS_KEY")
+                              :secret-key ~(System/getenv "AWS_SECRET_KEY")}
+                              :repositories [["s3-ovation-snapshot-repository" {:url        "s3p://maven.ovation.io/snapshot"
+                                                                                :username   :env/AWS_ACCESS_KEY
+                                                                                :passphrase :env/AWS_SECRET_KEY}]
+                                             ["s3-ovation-release-repository" {:url        "s3p://maven.ovation.io/release"
+                                                                               :username   :env/AWS_ACCESS_KEY
+                                                                               :passphrase :env/AWS_SECRET_KEY}]]
+                                 :local-repo ".repository"} }
 
-                       :jenkins {:aws          {:access-key ~(System/getenv "AWS_ACCESS_KEY")
-                                                :secret-key ~(System/getenv "AWS_SECRET_KEY")}
-                                 :repositories [["s3-ovation-snapshot-repository" {:url        "s3p://maven.ovation.io/snapshot"
-                                                                                   :username   :env/AWS_ACCESS_KEY
-                                                                                   :passphrase :env/AWS_SECRET_KEY}]
-                                                ["s3-ovation-release-repository" {:url        "s3p://maven.ovation.io/release"
-                                                                                  :username   :env/AWS_ACCESS_KEY
-                                                                                  :passphrase :env/AWS_SECRET_KEY}]]
-                                 :local-repo ".repository"}})
+  :aliases {"start-ovation-api-webservice"    ["with-profile" "ovation-web-api" "ring" "server"]}
+
+)
 
--- src/ovation_rest/handler.clj
+++ src/ovation_rest/handler.clj
@@ -1,47 +1,147 @@
 (ns ovation-rest.handler
-  (:use compojure.core
-        ring.middleware.params
-        )
-  (:require [compojure.handler :as handler]
-            [compojure.route :as route]
-            [ring.middleware.json :as middleware]
+  (:require [ring.util.http-response :refer :all]
+            [compojure.api.sweet :refer :all]
+            [ring.swagger.schema :refer [field describe]]
             [ovation-rest.entity :as entity]
             [ovation-rest.user :as user]
-            [ring.middleware.cors :refer [wrap-cors]]
-            )
+            [schema.core :as s]))
+
+;(ns ovation-rest.handler
+;  (:use compojure.core
+;        ring.middleware.params)
+;  (:require [compojure.handler :as handler]
+;            [compojure.route :as route]
+;            [ring.middleware.json :as middleware]
+;            [ovation-rest.entity :as entity]
+;            [ovation-rest.user :as user]
+;            [ring.middleware.cors :refer [wrap-cors]]
+;            [ring.util.http-response :refer :all]
+;            [compojure.api.sweet :refer :all]
+;            [schema.core :as s]))
+
+;{"_rev":"2-d330d60c5d22eeeb85dfb8ef4208055d",
+;"_id":"3dc9e647-9ecc-4fac-9f21-3d5cd08621e4",
+;"links":{"empty":false},
+;"named_links":{},
+;"annotations":{"properties":{"ovation://entities/1bb679e6-c853-4d73-8448-791e839f0077":[{"key":"key","value":"value"}]}},
+;"attributes":{"start":"2014-07-23T01:30:06.479Z",
+;              "name":"name","purpose":"purpose"},
+;"type":"Project"}
+
+(s/defschema Success {:success 1})
+
+(s/defschema Entity {
+;                     :type (s/enum :Project :Protocol :User :Source) 
+                     :_rev String
+                     :_id String
+                     :links {}
+                     :named_links {}
+                     :annotations {}
+                    }
+)
+
+
+(defapi app
+;  (middleware/wrap-json-response)
+;  (wrap-cors :access-control-allow-origin #".+"         ; FIXME - accept only what we want here
+;             :access-control-allow-methods [:get :put :post :delete :options]
+;             :access-control-allow-headers ["Content-Type" "Accept"])
+  (swagger-ui)
+  (swagger-docs
+    :title "ovation-api-webservice"
+    :description "Ovation API Webservice")
+
+  (swaggered "ovation-api-webservice"
+    :description "Ovation API Webservice"
+
+    (context "/api" []
+      (context "/entity" []
+        (POST* "/" request
+          :return        [Entity]
+          :query-params  [api-key :- String]
+          :summary       "Creates and returns an entity"
+          (ok (entity/create-entity request)))
+        (context "/:id" [id]
+          (GET* "/" request
+            :return        [Entity]
+            :query-params  [api-key :- String]
+            :summary       "Returns entity with :id"
+            (ok (entity/get-entity id request)))
+          (PUT* "/" request
+            :return        [Entity]
+            :query-params  [api-key :- String]
+            :summary       "Updates and returns updated entity with :id"
+            (ok (entity/update-entity id request)))
+          (DELETE* "/" request
+            :return        Success
+            :query-params  [api-key :- String]
+            :summary       "Deletes entity with :id"
+            (ok (entity/update-entity id request)))
+        )
+      )
+    )
+
+    (context "/:resource" [resource]
+      (GET* "/" request 
+        :return        [Entity]
+        :query-params  [api-key :- String]
+        :summary       "Special endpoint for /project /protocol /source"
+        (ok (entity/index-resource resource request)))
+    )
+
+;    (HEAD "/" [] "")
+
+;    (GET* "/" []
+;      :return         s/Str
+;      :query-params   []
+;      :summary        "Default return"
+;      (ok "")
+;    )
   )
+)
 
-(defroutes app-routes
+;      (context "/user" [] 
+;        (GET* "/" [] ;{params :params}
+;          :return       [{:user User}]
+;          :query-params [api-key :- String]
+;          :summary      "returns all Users"
+;          (ok {:user "all users"}))
+;;          (ok {:user (user/index-user params})))
+;        (GET* "/:id" [id] ;{params :params}
+;          :return User
+;          :query-params [api-key :- String]
+;          :summary      "returns a User"
+;          (ok {:user "one user"}))
+;;          (ok {:user (user/get-user params}))))
 
-           (context "/user" [] (defroutes index-routes
-                                          ; POST not allowed
-                                          (GET "/" {params :params} (user/index-user params))
-                                          (context "/:id" [id] (defroutes index-routes
-                                                                          ; PUT, DELETE not allowed
-                                                                          (GET "/" {params :params} (user/get-user params))))))
-
-           (context "/entity" [] (defroutes index-routes
-                                            (POST "/" request (entity/create-entity request))
-                                            (context "/:id" [id] (defroutes index-routes
-                                                                            (GET "/" request (entity/get-entity id request))
-                                                                            (PUT "/" request (entity/update-entity id request))
-                                                                            (DELETE "/" request (entity/delete-entity id request))
-                                                                            (context "/:rel" [rel] (defroutes index-routes
-                                                                                                              (GET "/" request (entity/get-entity-rel id rel request))))))))
-
-           (context "/:resource" [resource] (defroutes index-routes
-                                                       (GET "/" request (entity/index-resource resource request))))
-
-           ;; For ElasticBeanstalk
-           (HEAD "/" [] "")
-
-           (route/not-found "<h1>Not Found</h1>"))
-
-
-
-(def app
-  (-> (handler/site app-routes)
-      (middleware/wrap-json-response)
-      (wrap-cors :access-control-allow-origin #".+"         ; FIXME - accept only what we want here
-                 :access-control-allow-methods [:get :put :post :delete :options]
-                 :access-control-allow-headers ["Content-Type" "Accept"])))
+;(defroutes app-routes
+;
+;           (context "/user" [] (defroutes index-routes
+;                                          ; POST not allowed
+;                                          (GET "/" {params :params} (user/index-user params))
+;                                          (context "/:id" [id] (defroutes index-routes
+;                                                                          ; PUT, DELETE not allowed
+;                                                                          (GET "/" {params :params} (user/get-user params))))))
+;
+;           (context "/entity" [] (defroutes index-routes
+;                                   (POST "/" request (entity/create-entity request))
+;                                   (context "/:id" [id] (defroutes index-routes
+;                                    (GET "/" request (entity/get-entity id request))
+;                                    (PUT "/" request (entity/update-entity id request))
+;                                    (DELETE "/" request (entity/delete-entity id request))
+;                                    (context "/:rel" [rel] (defroutes index-routes
+;                                      (GET "/" request (entity/get-entity-rel id rel request))))))))
+;
+;           (context "/:resource" [resource] (defroutes index-routes
+;                                                       (GET "/" request (entity/index-resource resource request))))
+;
+;           (GET "/" [] "Ovation REST API")
+;           (route/not-found "<h1>Not Found</h1>"))
+;
+;
+;(def app
+;  (-> (handler/site app-routes)
+;      (middleware/wrap-json-response)
+;      (wrap-cors :access-control-allow-origin #".+"         ; FIXME - accept only what we want here
+;                 :access-control-allow-methods [:get :put :post :delete :options]
+;                 :access-contol-allow-headers ["Content-Type" "Accept"])))
