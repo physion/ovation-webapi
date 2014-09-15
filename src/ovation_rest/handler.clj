dd9587910d6c9f77934b158457585ff0f54842b3
Barry Wark <barrywark@gmail.com>
2014-09-15 01:47:33 -0400

    Ahh, it 'wraps'
    Dur-de-dur… you have to actually wrap the routes with the middlewares
    macro

--- src/ovation_rest/handler.clj
+++ src/ovation_rest/handler.clj
@@ -40,69 +40,67 @@
         (middlewares [(wrap-cors
                         :access-control-allow-origin #".+"  ; FIXME - accept only what we want here
                         :access-control-allow-methods [:get :put :post :delete :options]
-                        :access-control-allow-headers ["Content-Type" "Accept"])])
+                        :access-control-allow-headers ["Content-Type" "Accept"])]
+                     (swagger-ui)
+                     (swagger-docs
+                       :apiVersion "1.0.0"
+                       :title "Ovation"
+                       :description "Ovation Web API"
+                       :contact "support@ovation.io"
+                       :termsOfServicdUrl "https://ovation.io/terms_of_service")
 
-        {:formats [:application/json]}
+                     (swaggered "top-level"
+                                (context "/api" []
+                                         (context "/v1" []
+                                                  (context "/:resource" [resource]
+                                                           (GET* "/" request
+                                                                 :return [Entity]
+                                                                 :query-params [api-key :- String]
+                                                                 :path-params [resource :- (s/enum "projects" "sources" "protocols")]
+                                                                 :summary "Get Projects, Protocols and Top-level Sources"
 
+                                                                 (ok (entity/index-resource api-key resource (util/host-context request))))))))
 
-        (swagger-ui)
-        (swagger-docs
-          :apiVersion "1.0.0"
-          :title "Ovation"
-          :description "Ovation Web API"
-          :contact "support@ovation.io"
-          :termsOfServicdUrl "https://ovation.io/terms_of_service")
+                     (swaggered "entities"
+                                (context "/api" []
+                                         (context "/v1" []
+                                                  (context "/entities" []
+                                                           (POST* "/" request
+                                                                  :return [Entity]
+                                                                  :query-params [api-key :- String]
+                                                                  :body [new-dto NewEntity]
+                                                                  :summary "Creates and returns an entity"
+                                                                  (ok (entity/create-entity api-key new-dto (util/host-context request))))
+                                                           (context "/:id" [id]
+                                                                    (GET* "/" request
+                                                                          :return [Entity]
+                                                                          :query-params [api-key :- String]
+                                                                          :summary "Returns entity with :id"
+                                                                          (ok (entity/get-entity api-key id (util/host-context request :remove-levels 1))))
+                                                                    (PUT* "/" request
+                                                                          :return [Entity]
+                                                                          :query-params [api-key :- String]
+                                                                          :body [dto Entity]
+                                                                          :summary "Updates and returns updated entity with :id"
+                                                                          (ok (entity/update-entity api-key id dto (util/host-context request :remove-levels 1))))
+                                                                    (DELETE* "/" request
+                                                                             :return Success
+                                                                             :query-params [api-key :- String]
+                                                                             :summary "Deletes entity with :id"
+                                                                             (ok (entity/delete-entity api-key id))))))))
 
-        (swaggered "top-level"
-                   (context "/api" []
-                            (context "/v1" []
-                                     (context "/:resource" [resource]
-                                              (GET* "/" request
-                                                    :return [Entity]
-                                                    :query-params [api-key :- String]
-                                                    :path-params [resource :- (s/enum "projects" "sources" "protocols")]
-                                                    :summary "Get Projects, Protocols and Top-level Sources"
+                     (swaggered "views"
+                                (context "/api" []
+                                         (context "/v1" []
+                                                  (context "/views" []
+                                                           (GET* "/*" request
+                                                                 :return [Entity]
+                                                                 :query-params [api-key :- String]
+                                                                 :summary "Returns entities in view. Views follow CouchDB calling conventions (http://wiki.apache.org/couchdb/HTTP_view_API)"
+                                                                 (let [host (util/host-from-request request)]
+                                                                   (ok (entity/get-view api-key
+                                                                                        (url-normalize (format "%s/%s?%s" host (:uri request) (util/ovation-query request)))
+                                                                                        (util/host-context request :remove-levels 1))))))))))
 
-                                                    (ok (entity/index-resource api-key resource (util/host-context request))))))))
-
-        (swaggered "entities"
-                   (context "/api" []
-                            (context "/v1" []
-                                     (context "/entities" []
-                                              (POST* "/" request
-                                                     :return [Entity]
-                                                     :query-params [api-key :- String]
-                                                     :body [new-dto NewEntity]
-                                                     :summary "Creates and returns an entity"
-                                                     (ok (entity/create-entity api-key new-dto (util/host-context request))))
-                                              (context "/:id" [id]
-                                                       (GET* "/" request
-                                                             :return [Entity]
-                                                             :query-params [api-key :- String]
-                                                             :summary "Returns entity with :id"
-                                                             (ok (entity/get-entity api-key id (util/host-context request :remove-levels 1))))
-                                                       (PUT* "/" request
-                                                             :return [Entity]
-                                                             :query-params [api-key :- String]
-                                                             :body [dto Entity]
-                                                             :summary "Updates and returns updated entity with :id"
-                                                             (ok (entity/update-entity api-key id dto (util/host-context request :remove-levels 1))))
-                                                       (DELETE* "/" request
-                                                                :return Success
-                                                                :query-params [api-key :- String]
-                                                                :summary "Deletes entity with :id"
-                                                                (ok (entity/delete-entity api-key id))))))))
-
-        (swaggered "views"
-                   (context "/api" []
-                            (context "/v1" []
-                                     (context "/views" []
-                                              (GET* "/*" request
-                                                    :return [Entity]
-                                                    :query-params [api-key :- String]
-                                                    :summary "Returns entities in view. Views follow CouchDB calling conventions (http://wiki.apache.org/couchdb/HTTP_view_API)"
-                                                    (let [host (util/host-from-request request)]
-                                                      (ok (entity/get-view api-key
-                                                                           (url-normalize (format "%s/%s?%s" host (:uri request) (util/ovation-query request)))
-                                                                           (util/host-context request :remove-levels 1))))))))))
+        {:formats [:application/json]})
 
