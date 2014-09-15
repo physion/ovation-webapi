675ef1185c43f584f74aca63006077ed0c72d744
Barry Wark <barrywark@gmail.com>
2014-09-14 23:28:56 -0400

    Trying with ring-cors

--- src/ovation_rest/handler.clj
+++ src/ovation_rest/handler.clj
@@ -35,9 +35,9 @@
 
 ;;; --- Middleware --- ;;;
 (defn wrap-cors [handler]
-  (ring.middleware.cors/wrap-cors handler #".*")) ; FIXME - accept only what we want here
-                                  ;:access-control-allow-origin #".+" ; FIXME - accept only what we want here
-                                  ;:access-control-allow-methods [:get :put :post :delete :options]
+  (ring.middleware.cors/wrap-cors handler
+                                  :access-control-allow-origin #".+" ; FIXME - accept only what we want here
+                                  :access-control-allow-methods [:get :put :post :delete :options]))
                                   ;:access-control-allow-headers ["Content-Type" "Accept"]))
 
 
