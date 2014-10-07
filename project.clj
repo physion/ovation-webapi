383c36eb1b63bfe046f5049d0f261b0a1edd7051
Barry Wark <barrywark@gmail.com>
2014-10-07 14:27:12 -0400

    Let's try using newrelic-agent via dependency

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
                            [metosin/ring-swagger "0.13.0"]
                            [metosin/ring-swagger-ui "2.0.17"]
                            [metosin/compojure-api "0.16.2"]
-                           ;[com.newrelic.agent.java/newrelic-agent "3.9.0"]
+                           [com.newrelic.agent.java/newrelic-agent "3.9.0"]
                            [com.google.guava/guava "13.0.1"]
                            [clojurewerkz/urly "1.0.0"]
                            [pathetic "0.5.1"]]
@@ -33,17 +33,19 @@
             :ring {:handler ovation-rest.handler/app}
 
             :aws {:beanstalk {:stack-name   "64bit Amazon Linux running Tomcat 7"
-                              :environments [{:name "development"
-                                              :env  {"OVATION_IO_HOST_URI" "https://dev.ovation.io"}}
+                              :environments [{:name  "webapi-development"
+                                              :alias "development"
+                                              :env   {"OVATION_IO_HOST_URI" "https://dev.ovation.io"}}
 
                                              {:name "staging"
                                               :env  {"OVATION_IO_HOST_URI" "https://ovation.io"}}
 
-                                             {:name "production"
-                                              :env  {"OVATION_IO_HOST_URI" "https://ovation.io"}}]}}
+                                             {:name  "webapi-production"
+                                              :alias "production"
+                                              :env   {"OVATION_IO_HOST_URI" "https://ovation.io"}}]}}
 
             :profiles {
-                        :ovation-web-api {:ring {:handler ovation-rest.handler/app}
+                        :ovation-web-api {:ring         {:handler ovation-rest.handler/app}
                                           :reload-paths ["src"]}
                         :dev             {:dependencies [[javax.servlet/servlet-api "2.5"]
                                                          [ring-mock "0.1.5"]
