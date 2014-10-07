f1cf7f28178a8c8015c691a3d81001830c2b9836
Barry Wark <barrywark@gmail.com>
2014-10-06 23:54:47 -0400

    Adds annotation schemata and prototype endpoint

--- src/ovation_rest/handler.clj
+++ src/ovation_rest/handler.clj
@@ -30,84 +30,128 @@
 
 (s/defschema EntityList [Entity])
 
+(def NewAnnotationBase {:user   s/Str
+                        :entity s/Str})
+
+(def AnnotationMeta {:_id  s/Str
+                     :_rev s/Str})
+
+(s/defschema NewTagRecord (assoc NewAnnotationBase :annotation_type "keywords"
+                                                   :annotation {:tag s/Str}))
+(s/defschema TagRecord (conj NewTagRecord AnnotationMeta))
+
+
+(s/defschema NewPropertyRecord (assoc NewAnnotationBase :annotation_type "properties"
+                                                        :annotation {:key   s/Str
+                                                                     :value s/Any}))
+(s/defschema PropertyRecord (conj NewPropertyRecord AnnotationMeta))
+
+
+(s/defschema NewNoteRecord (assoc NewAnnotationBase :annotation_type "notes"
+                                                    :annotation {:text      s/Str
+                                                                 :timestamp s/Str}))
+(s/defschema NoteRecord (conj NewNoteRecord AnnotationMeta))
+
+
+(s/defschema NewTimelineEventRecord (assoc NewAnnotationBase :annotation_type "timeline_events"
+                                                             :annotation {:name  s/Str
+                                                                          :notes s/Str
+                                                                          :start s/Str
+                                                                          :end   s/Str}))
+(s/defschema TimelineEventRecord (conj NewTimelineEventRecord AnnotationMeta))
+
+
+
+(s/defschema NewAnnotation (or NewTagRecord NewPropertyRecord NewNoteRecord NewTimelineEventRecord))
+(s/defschema Annotation (or TagRecord PropertyRecord NoteRecord TimelineEventRecord))
+
+
+
 
 ;;; --- Routes --- ;;;
 (defapi app
 
-(middlewares [(wrap-cors
-                :access-control-allow-origin #".+"  ; FIXME - accept only what we want here
-                :access-control-allow-methods [:get :put :post :delete :options]
-                :access-control-allow-headers ["Content-Type" "Accept"])]
-(swagger-ui)
-(swagger-docs
- :apiVersion "1.0.0"
- :title "Ovation"
- :description "Ovation Web API"
- :contact "support@ovation.io"
- :termsOfServiceUrl "https://ovation.io/terms_of_service")
+  (middlewares [(wrap-cors
+                  :access-control-allow-origin #".+"        ; FIXME - accept only what we want here
+                  :access-control-allow-methods [:get :put :post :delete :options]
+                  :access-control-allow-headers ["Content-Type" "Accept"])]
+    (swagger-ui)
+    (swagger-docs
+      :apiVersion "1.0.0"
+      :title "Ovation"
+      :description "Ovation Web API"
+      :contact "support@ovation.io"
+      :termsOfServiceUrl "https://ovation.io/terms_of_service")
 
-(swaggered "top-level"
-          (context "/api" []
-                   (context "/v1" []
-                            (context "/:resource" [resource]
-                                     (GET* "/" request
-                                           :return [Entity]
-                                           :query-params [api-key :- String]
-                                           :path-params [resource :- (s/enum "projects" "sources" "protocols")]
-                                           :summary "Get Projects, Protocols and Top-level Sources"
+    (swaggered "top-level"
+      (context "/api" []
+        (context "/v1" []
+          (context "/:resource" [resource]
+            (GET* "/" request
+              :return [Entity]
+              :query-params [api-key :- String]
+              :path-params [resource :- (s/enum "projects" "sources" "protocols")]
+              :summary "Get Projects, Protocols and Top-level Sources"
 
-                                           (ok (entity/index-resource api-key resource)))))))
+              (ok (entity/index-resource api-key resource)))))))
 
-(swaggered "entities"
-          (context "/api" []
-                   (context "/v1" []
-                            (context "/entities" []
-                                     (POST* "/" request
-                                            :return [Entity]
-                                            :query-params [api-key :- String]
-                                            :body [new-dto NewEntity]
-                                            :summary "Creates and returns an entity"
-                                            (ok (entity/create-entity api-key new-dto)))
-                                     (context "/:id" [id]
-                                              (GET* "/" request
-                                                    :return [Entity]
-                                                    :query-params [api-key :- String]
-                                                    :summary "Returns entity with :id"
-                                                    (ok (entity/get-entity api-key id)))
-;                                              (PUT* "/" request
-;                                                    :return [Entity]
-;                                                    :query-params [api-key :- String]
-;                                                    :body [dto Entity]
-;                                                    :summary "Updates and returns updated entity with :id"
-;                                                    (ok (entity/update-entity api-key id dto (util/host-context request :remove-levels 1))))
-                                              (DELETE* "/" request
-                                                       :return Success
-                                                       :query-params [api-key :- String]
-                                                       :summary "Deletes entity with :id"
-                                                       (ok (entity/delete-entity api-key id)))
-                                       (context "/links/:rel" [rel]
-                                          (GET* "/" request
-                                            :return [Entity]
-                                            :query-params [api-key :- String]
-                                            :summary "Returns all entities associated with entity/rel"
-                                            (ok (entity/get-entity-link api-key id rel))))
-                                       (context "/named_links/:rel/:named" [rel named]
-                                          (GET* "/" request
-                                            :return [Entity]
-                                            :query-params [api-key :- String]
-                                            :summary "Returns all entities associated with entity/rel/named"
-                                            (ok (entity/get-entity-named-link api-key id rel named)))))))))
+    (swaggered "entities"
+      (context "/api" []
+        (context "/v1" []
+          (context "/entities" []
+            (POST* "/" request
+              :return [Entity]
+              :query-params [api-key :- String]
+              :body [new-dto NewEntity]
+              :summary "Creates and returns an entity"
+              (ok (entity/create-entity api-key new-dto)))
+            (context "/:id" [id]
+              (GET* "/" request
+                :return [Entity]
+                :query-params [api-key :- String]
+                :summary "Returns entity with :id"
+                (ok (entity/get-entity api-key id)))
+              (context "/annotations" []
+                (POST* "/" request
+                  :return Annotation
+                  :query-params [api-key :- String]
+                  :body [new-annotation NewAnnotation]
+                  :summary "Adds a new annotation (owned by current authenticated user) to this entity"
+                  nil))
+              ;                                              (PUT* "/" request
+              ;                                                    :return [Entity]
+              ;                                                    :query-params [api-key :- String]
+              ;                                                    :body [dto Entity]
+              ;                                                    :summary "Updates and returns updated entity with :id"
+              ;                                                    (ok (entity/update-entity api-key id dto (util/host-context request :remove-levels 1))))
+              (DELETE* "/" request
+                :return Success
+                :query-params [api-key :- String]
+                :summary "Deletes entity with :id"
+                (ok (entity/delete-entity api-key id)))
+              (context "/links/:rel" [rel]
+                (GET* "/" request
+                  :return [Entity]
+                  :query-params [api-key :- String]
+                  :summary "Returns all entities associated with entity/rel"
+                  (ok (entity/get-entity-link api-key id rel))))
+              (context "/named_links/:rel/:named" [rel named]
+                (GET* "/" request
+                  :return [Entity]
+                  :query-params [api-key :- String]
+                  :summary "Returns all entities associated with entity/rel/named"
+                  (ok (entity/get-entity-named-link api-key id rel named)))))))))
 
-(swaggered "views"
-          (context "/api" []
-                   (context "/v1" []
-                            (context "/views" []
-                                     (GET* "/*" request
-                                           :return [Entity]
-                                           :query-params [api-key :- String]
-                                           :summary "Returns entities in view. Views follow CouchDB calling conventions (http://wiki.apache.org/couchdb/HTTP_view_API)"
-                                           (let [host (util/host-from-request request)]
-                                             (ok (entity/get-view api-key
-                                                                  (url-normalize (format "%s/%s?%s" host (:uri request) (util/ovation-query request)))
-                                                                  (util/host-context request :remove-levels 1)))))))))))
+    (swaggered "views"
+      (context "/api" []
+        (context "/v1" []
+          (context "/views" []
+            (GET* "/*" request
+              :return [Entity]
+              :query-params [api-key :- String]
+              :summary "Returns entities in view. Views follow CouchDB calling conventions (http://wiki.apache.org/couchdb/HTTP_view_API)"
+              (let [host (util/host-from-request request)]
+                (ok (entity/get-view api-key
+                      (url-normalize (format "%s/%s?%s" host (:uri request) (util/ovation-query request)))
+                      (util/host-context request :remove-levels 1)))))))))))
 
