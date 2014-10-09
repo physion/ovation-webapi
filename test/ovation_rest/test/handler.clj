c6484406fbedb8d3f0f8c15a889a4d93d5118b5b
Barry Wark <barrywark@gmail.com>
2014-10-09 15:00:00 -0400

    Adds schema coverage for POST /entities

--- test/ovation_rest/test/handler.clj
+++ test/ovation_rest/test/handler.clj
@@ -1,21 +1,41 @@
 (ns ovation-rest.test.handler
   (:require [midje.sweet :refer :all]
-            [ring.mock.request :refer :all]
-            [ovation-rest.handler :as handler]))
+            [ring.mock.request :as mock]
+            [ovation-rest.handler :as handler]
+            [clojure.data.json :as json]
+            [ovation-rest.util :as util]
+            [ovation-rest.entity :as entity]
+            [clojure.walk :as walk]))
 
-(facts "about doc route"
-       (fact "HEAD / => 302"
-             (let [response (handler/app (request :head "/"))]
-               (:status response) => 302))
-       (fact "GET / redirects"
-             (let [response (handler/app (request :get "/"))]
-               (:status response) => 302
-               (-> response (:headers) (get "Location")) => "/index.html"))
-       (fact "HEAD /index.html returns 200"
-             (let [response (handler/app (request :head "/index.html"))]
-               (:status response) => 200)))
+(facts "About doc route"
+  (fact "HEAD / => 302"
+    (let [response (handler/app (mock/request :head "/"))]
+      (:status response) => 302))
+  (fact "GET / redirects"
+    (let [response (handler/app (mock/request :get "/"))]
+      (:status response) => 302
+      (-> response (:headers) (get "Location")) => "/index.html"))
+  (fact "HEAD /index.html returns 200"
+    (let [response (handler/app (mock/request :head "/index.html"))]
+      (:status response) => 200)))
 
-(facts "about invalid routes"
-       (fact "returns 404 for invalid path"
-             (let [response (handler/app (request :get "/invalid/path?api-key=123"))]
-               response => nil?)))
+(facts "About invalid routes"
+  (fact "returns 404 for invalid path"
+    (let [response (handler/app (mock/request :get "/invalid/path"))]
+      response => nil?)))
+
+(facts "About entities"
+  (fact "POST /entities inserts an entity (201)"
+    (let [dto {:type       "Project"
+               :attributes {}}
+          post (-> (mock/request :post "/api/v1/entities")
+                 (mock/query-string {"api-key" "12345"})
+                 (mock/content-type "application/json")
+                 (mock/body (json/write-str (walk/stringify-keys dto))))]
+      (:status (handler/app post)) => 201
+      (provided
+        (entity/create-entity "12345" {:type       "Project"
+                                       :attributes {}}) => [{:_id        "id"
+                                                             :_rev       "rev"
+                                                             :attributes {}
+                                                             :type       "Project"}]))))
