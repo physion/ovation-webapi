5b8d45acc14e0f7bebea3534c90d569209ceb89f
Barry Wark <barrywark@gmail.com>
2015-09-08 16:51:10 -0400

    post-resources should allow any of supplied schemas

--- src/ovation/route_helpers.clj
+++ src/ovation/route_helpers.clj
@@ -78,7 +78,7 @@
         type-kw (keyword type-path)]
     `(POST* "/" request#
        :return {~type-kw [~(clojure.core/symbol "ovation.schema" type-name)]}
-       :body [entities# ~schemas]
+       :body [entities# [(apply s/either ~schemas)]]
        :summary ~(str "Creates a new top-level " type-name)
        (post-resources* request# ~type-name ~type-kw entities#))))
 
