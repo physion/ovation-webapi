dc6a841df2128b487a123f0688056fb6b26f5cbb
Barry Wark <barrywark@gmail.com>
2015-11-05 16:47:44 -0500

    Update dips

--- project.clj
+++ project.clj
@@ -3,33 +3,33 @@
   :url "http://ovation.io"
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.async "0.2.371"]
 
                  ;; Compojure API and middleware
                  [metosin/compojure-api "0.23.1"]
-                 [metosin/ring-swagger-ui "2.1.2"]
+                 [metosin/ring-swagger-ui "2.1.3"]
                  [ring-cors "0.1.7"]
 
                  ;; HTTP and CouchDB
-                 [http-kit "2.1.18"]
+                 [http-kit "2.1.19"]
                  [org.clojure/data.codec "0.1.0"]
                  [com.ashafa/clutch "0.4.0"]
 
                  ;; New Relic agent (JAR)
-                 [com.newrelic.agent.java/newrelic-agent "3.11.0"]
+                 [com.newrelic.agent.java/newrelic-agent "3.21.0"]
 
 
                  ;; Logging
                  [org.clojure/tools.logging "0.3.1"]
                  [clj-logging-config "1.9.12"]
-                 [org.slf4j/slf4j-api "1.7.7"]
-                 [org.slf4j/slf4j-log4j12 "1.7.7"]
+                 [org.slf4j/slf4j-api "1.7.12"]
+                 [org.slf4j/slf4j-log4j12 "1.7.12"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jmdk/jmxtools
                                                     com.sun.jmx/jmxri]]
                  [ring.middleware.logger "0.5.0"]
-                 [ring.middleware.conditional "0.1.0"]
+                 [ring.middleware.conditional "0.2.0"]
 
                  ;; Other
                  [org.clojure/data.json "0.2.6"]
@@ -45,7 +45,7 @@
   :resource-paths ["resources"]
 
   ;; For New Relic, we need to bundle newrelic.yml and newrelic.jar
-  :war-resources-path "war_resources"
+  :war-resources-path "war-resources"
 
   :aws {:beanstalk {:stack-name   "64bit Amazon Linux running Tomcat 7"
                     :environments [{:name "webapi-development"
@@ -62,12 +62,11 @@
                                :reload-paths ["src"]}
              :dev             {:dependencies [[javax.servlet/servlet-api "2.5"]
                                               [ring-mock "0.1.5"]
-                                              [midje "1.7.0"]
-                                              [org.clojure/data.json "0.2.5"]
-                                              [http-kit.fake "0.2.1"]
+                                              [midje "1.8.1"]
+                                              [http-kit.fake "0.2.2"]
                                               [ring-serve "0.1.2"]]
-                               :plugins      [[lein-midje "3.1.3"]
-                                              [lein-ring "0.9.6"]]}
+                               :plugins      [[lein-midje "3.2"]
+                                              [lein-ring "0.9.7"]]}
              :jenkins         {:aws {:access-key ~(System/getenv "AWS_ACCESS_KEY")
                                      :secret-key ~(System/getenv "AWS_SECRET_KEY")}}}
 
