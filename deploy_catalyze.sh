8e5d3cffea2eb8c4309b8fd2dce6f8d962559a31
Barry Wark <barrywark@gmail.com>
2016-04-11 01:41:56 -0400

    Update catalyze deploy

--- deploy_catalyze.sh
+++ deploy_catalyze.sh
@@ -7,4 +7,6 @@ tar xzvf catalyze_3.1.5_linux_amd64.tar.gz
 # Associate catalyze environment
 catalyze_3.1.5_linux_amd64/catalyze associate $CATALYZE_ENVIRONMENT app --username $CATALYZE_USER  --password $CATALYZE_PASSWORD
 
+# Deploy
+git fetch --unshallow || true
 git push catalyze ${CI_COMMIT_ID}:master
