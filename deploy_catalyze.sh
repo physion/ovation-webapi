5a3352d5a7ef3abdad9386b66fb8b7dccf9aa68d
Barry Wark <barrywark@gmail.com>
2016-04-14 14:38:35 -0400

    Don't download if catalyze already installed

--- deploy_catalyze.sh
+++ deploy_catalyze.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# Install catalyze-cli
-wget https://github.com/catalyzeio/cli/releases/download/3.1.5/catalyze_3.1.5_linux_amd64.tar.gz
-tar xzvf catalyze_3.1.5_linux_amd64.tar.gz
+CATALYZE_CLI=catalyze_3.1.5_linux_amd64
+CATALYZE_VERISON=3.1.5
+
+if [ ! -d "$CATALYZE_CLI" ]; then
+    # Install catalyze-cli
+    wget https://github.com/catalyzeio/cli/releases/download/$CATALYZE_VERSION/$CATALYZE_CLI.tar.gz
+    tar xzvf $CATALYZE_CLI.tar.gz
+fi
+
 
 # Associate catalyze environment
-catalyze_3.1.5_linux_amd64/catalyze  --username=$CATALYZE_USER  --password=$CATALYZE_PASSWORD associate $CATALYZE_ENVIRONMENT app
+$CATALYZE_CLI/catalyze  --username=$CATALYZE_USER  --password=$CATALYZE_PASSWORD associate $CATALYZE_ENVIRONMENT app
 
 # Deploy
 git fetch --unshallow || true
