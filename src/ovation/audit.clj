cc16ad85d587f717130fa36162a943f1200b576d
Barry Wark <barrywark@gmail.com>
2016-04-14 23:23:32 -0400

    Clean up AUDIT request details logging

--- src/ovation/audit.clj
+++ src/ovation/audit.clj
@@ -1,16 +1,17 @@
 (ns ovation.audit
   (:require [ring.logger.messages :refer [request-details]]
-            [ovation.logging :refer [info]]
+            [ring.logger.protocols :refer [info]]
+            [ovation.logging]
             [ovation.auth :as auth]))
 
 
 (defmethod request-details :identity-printer
   [{:keys [logger] :as options} req]
-  (ovation.logging/info "[AUDIT]" (merge {:identity (get-in req [:identity :uuid])}
-                                    (select-keys req [:request-method
-                                                      :uri]))))
+  (info logger (str "[AUDIT] " (merge {:identity (get-in req [:identity :uuid])}
+                                (select-keys req [:request-method
+                                                  :uri])))))
 
 (defn audit
   "Logs audit for request"
   [request body]
-  (info "[AUDIT]" {:identity (auth/authenticated-user-id (auth/identity request))} body))
+  (ovation.logging/info "[AUDIT] " {:identity (auth/authenticated-user-id (auth/identity request))} body))
