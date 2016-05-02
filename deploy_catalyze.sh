4d7cd500f1e2f5140cb450e1f2d5111b3a5086ec
Barry Wark <barrywark@gmail.com>
2016-05-01 21:33:30 -0400

    Adds git-only deploy to catalyze

--- deploy_catalyze.sh
+++ deploy_catalyze.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-VERISON=3.1.5
-CATALYZE_CLI=catalyze_${VERISON}_linux_amd64
-
-if [ ! -d "$CATALYZE_CLI" ]; then
-    # Install catalyze-cli
-    wget https://github.com/catalyzeio/cli/releases/download/$VERISON/$CATALYZE_CLI.tar.gz
-    tar xzvf $CATALYZE_CLI.tar.gz
-fi
-
-
 # Associate catalyze environment
-$CATALYZE_CLI/catalyze  --username=$CATALYZE_USER  --password=$CATALYZE_PASSWORD associate $CATALYZE_ENVIRONMENT app
+git remote add catalyze ssh://git@git.pod02.catalyzeapps.com:2222/pod02234-code-927176715.git
 
 # Deploy
 git fetch --unshallow || true
