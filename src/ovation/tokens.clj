6553abac7399302871422ccbe7597a9e55cc6757
Barry Wark <barrywark@gmail.com>
2016-05-13 13:22:03 -0400

    Working tokens/get-token and handler endpoint

--- src/ovation/tokens.clj
+++ src/ovation/tokens.clj
@@ -10,13 +10,15 @@
   [email password]
   (let [body    (json/write-str (walk/stringify-keys {:email email :password password}))
         options {:body body
-                 :accept "application/json; charset=utf-8"
-                 :conent-type "application/json; charset=utf-8"}
+                 :headers {"Content-Type" "application/json"}}
         url (util/join-path [config/AUTH_SERVER "api" "v1" "sessions"])
-        _ (println url)
-        response (http/post url options)]
-    (println @response)
-    @response))
+        response (http/post url options)
+        body-json (walk/keywordize-keys (json/read-str (:body @response)))
+        status (int (:status @response))]
+    (-> @response
+      (dissoc :opts)
+      (assoc :status status)
+      (assoc :body body-json))))
 
 (defn refresh-token
   [request]
