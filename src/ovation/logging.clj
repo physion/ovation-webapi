6609e7047490076baafeab3ef063a336deb50b9c
Barry Wark <barrywark@gmail.com>
2016-05-26 00:32:57 -0400

    Try again log stash appender

--- src/ovation/logging.clj
+++ src/ovation/logging.clj
@@ -1,5 +1,6 @@
 (ns ovation.logging
   (:require [taoensso.timbre :as timbre]
+            ;[taoensso.timbre.appenders.3rd-party.logstash :as logstash-appender]
             [potemkin :refer [import-vars]]
             [ovation.config :as config]
             [cheshire.core :as cheshire]
@@ -8,16 +9,14 @@
   (:import [java.net Socket InetAddress]
            [java.io PrintWriter]))
 
+
 (import-vars
   [taoensso.timbre
    log debug info warn error fatal])
 
 
+;; copied from taoensso.timbre.appenders.3rd-party.logstash until it's in the JAR
 
-
-
-;; Adapted from taoensso.timbre.appenders.3rd-party.server-socket
-;; Adapted from active-group/timbre-logstash
 (defn connect
   [host port]
   (let [addr (InetAddress/getByName host)
@@ -31,28 +30,38 @@
     (.isConnected sock)
     (not (.checkError out))))
 
-(def iso-formatter (time-format/formatters :basic-date-time))
+(def iso-format "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 
-(defn data->json-string
-  [data]
-  (cheshire/generate-string
-    (merge (:context data)
-      {:throwable    (some-> (:throwable data) timbre/stacktrace)
-       :level        (:level data)
-       :error-level? (:error-level? data)
-       :?ns-str      (:?ns-str data)
-       :?file        (:?file data)
-       :?line        (:?line data)
-       :err          (str (force (:?err_ data)))
-       :vargs        (force (:vargs_ data))
-       :hostname     (force (:hostname_ data))
-       :msg          (force (:msg_ data))
-       :timestamp    (time-format/unparse iso-formatter (time-coerce/from-date (:instant data)))})))
+(defn data->json-stream
+  [data writer opts]
+  ;; Note: this it meant to target the logstash-filter-json; especially "message" and "@timestamp" get a special meaning there.
+  (let [stacktrace-str (if-let [pr (:pr-stacktrace opts)]
+                         #(with-out-str (pr %))
+                         timbre/stacktrace)]
+    (cheshire/generate-stream
+      (merge (:context data)
+        {:level (:level data)
+         :namespace (:?ns-str data)
+         :file (:?file data)
+         :line (:?line data)
+         :stacktrace (some-> (force (:?err_ data)) (stacktrace-str))
+         :hostname (force (:hostname_ data))
+         :message (force (:msg_ data))
+         "@timestamp" (:instant data)})
+      writer
+      (merge {:date-format iso-format
+              :pretty false}
+        opts))))
 
-(defn timbre-json-appender
-  "Returns a Logstash appender."
-  [host port]
-  (let [conn (atom nil)]
+(defn logstash-appender
+  "Returns a Logstash appender, which will send each event in JSON
+  format to the logstash server at `host:port`. Additionally `opts`
+  may be a map with `:pr-stracktrace` mapped to a function taking an
+  exception, which should write the stacktrace of that exception to
+  `*out`."
+  [host port & [opts]]
+  (let [conn (atom nil)
+        nl "\n"]
     {:enabled?   true
      :async?     false
      :min-level  nil
@@ -64,10 +73,11 @@
                      (let [[sock out] (swap! conn
                                         (fn [conn]
                                           (or (and conn (connection-ok? conn) conn)
-                                            (connect host port))))
-                           json (data->json-string data)]
-                       (binding [*out* out]
-                         (println json)))
+                                            (connect host port))))]
+                       (locking sock
+                         (data->json-stream data out (select-keys opts [:pr-stacktrace]))
+                         ;; logstash tcp input plugin: "each event is assumed to be one line of text".
+                         (.write ^java.io.Writer out nl)))
                      (catch java.io.IOException _
                        nil)))}))
 
@@ -81,7 +91,7 @@
                    :println (timbre/println-appender {:stream :auto})}})
     {:level     :info}))
      ;:appenders {:println (timbre/println-appender {:stream :auto})}
-     
+
 
 (defn setup! []
   (timbre/set-config! (logging-config)))
