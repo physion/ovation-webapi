8ba383bef053146ddfdab327d9a82d0ee298a149
Barry Wark <barrywark@gmail.com>
2016-06-02 10:36:48 -0400

    Adds search function for Cloudant Search

---
+++ js/search.js
@@ -0,0 +1,31 @@
+/* {
+  "_id": "_design/search",
+  "indexes": {
+    "all": {
+      "index": "function(doc){ ... }"
+    }
+  }
+}
+*/
+
+function(doc) {
+  if(doc.type) {
+    index("type", doc.type, {"store": true});
+  }
+
+  if(doc.attributes) {
+    for(var k1 in doc.attributes) {
+      index(k1, doc.attributes[k1], {"store": true});
+      index("default", doc.attributes[k1]);
+    }
+    index("id", doc._id, {"store": true});
+  }
+
+  if(doc.type === 'Annotation') {
+    for(var k2 in doc.annotation) {
+      index(k2, doc.annotation[k2], {"store": true});
+      index("default", doc.annotation[k2]);
+    }
+    index("id", doc.entity, {"store": true});
+  }
+}
