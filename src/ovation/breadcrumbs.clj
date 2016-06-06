6ba77d9d5d6bdcb77b5a56111e20588a5cb020f9
Barry Wark <barrywark@gmail.com>
2016-06-06 10:25:55 -0400

    WIP

--- src/ovation/breadcrumbs.clj
+++ src/ovation/breadcrumbs.clj
@@ -8,13 +8,18 @@
             [ovation.util :as util]))
 
 
+(defn get-parents
+  [auth routes id]
+  (links/get-link-targets auth id k/PARENTS-REL routes))
+
+
 (defn build-graph
   "Builds a directed graph of child -> parent nodes using loop/recur. Returns the Ubergraph."
   [auth routes entity-ids graph]
   (loop [ids entity-ids
          g   graph]
     (let [edges        (pmap (fn [id]
-                               (let [parents (links/get-link-targets auth id k/PARENTS-REL routes)]
+                               (let [parents (get-parents auth id routes)]
                                  (map (fn [parent] [id (:_id parent)]) parents))) ids)
           parent-edges (apply concat edges)
           parent-ids   (set (map #(second %) parent-edges))]
@@ -23,22 +28,46 @@
         g
         (recur parent-ids (uber/add-edges* g parent-edges))))))
 
+(defn make-node-description
+  [id entities]
+  (let [e (get entities id {})]
+    {:type (:type e)
+     :id id
+     :name (get-in e [:attributes :name])}))
+
+(defn- extend-path
+  [id spans entities path]
+  (let [head (-> path last :id)
+        next-nodes (get-in spans [id head])]
+    (if (nil? next-nodes)
+      path
+      (let [result (map (fn [n]
+                          (conj path (make-node-description n entities))) next-nodes)]
+        result))))
+
+(defn- extend-paths
+  [id spans entities starting-paths]
+  (loop [paths starting-paths]
+    (let [
+          _ (println paths)
+          head-nodes (map (fn [n] (-> n last :id)) paths)
+          next-nodes (doall (map #(get-in spans [id %]) head-nodes))]
+         (if (every? nil? next-nodes)
+           paths
+           (recur (map #(extend-path id spans entities %) paths))))))
+
+
+
 (defn collect-paths
   "Finds all paths from ids to parents"
   [auth graph ids routes]
   (let [entities (util/into-id-map (core/get-entities auth (uber/nodes graph) routes))
         spans (apply conj (map (fn [start]
-                                 (let [span (alg/pre-span graph start)
-                                       _    (pprint span)]
+                                 (let [span (alg/pre-span graph start)]
                                    {start span}))
                              ids))]
-    (println spans)))
+    (into {} (map (fn [id] [id (extend-paths id spans entities [[(make-node-description id entities)]])]) ids))))
 
-
-
-(defn get-parents
-  [auth routes doc]
-  (links/get-link-targets auth (:_id doc) k/PARENTS-REL routes))
 
 (defn get-breadcrumbs
   "Gets all breadcrumb paths to entities with IDs `ids`"
--- test/ovation/test/breadcrumbs.clj
+++ test/ovation/test/breadcrumbs.clj
@@ -9,35 +9,35 @@
 (facts "About parents"
   (facts "all entities"
     (fact "is parents"
-      (b/get-parents ..auth.. ..rt.. {:_id ..id..}) => ..parents..
+      (b/get-parents ..auth.. ..rt.. ..id..) => ..parents..
       (provided
         (links/get-link-targets ..auth.. ..id.. "parents" ..rt..) => ..parents..))))
 
 (facts "About breadcrumbs"
-  (against-background [(links/get-link-targets ..auth.. ..file1.. k/PARENTS-REL ..rt..) => [{:_id ..folder1..} {:_id ..folder2..}]
-                       (links/get-link-targets ..auth.. ..file2.. k/PARENTS-REL ..rt..) => [{:_id ..folder2..}]
-                       (links/get-link-targets ..auth.. ..folder1.. k/PARENTS-REL ..rt..) => [{:_id ..project1..}]
-                       (links/get-link-targets ..auth.. ..folder2.. k/PARENTS-REL ..rt..) => [{:_id ..project1..} {:_id ..project2..}]
-                       (links/get-link-targets ..auth.. ..project1.. k/PARENTS-REL ..rt..) => []
-                       (links/get-link-targets ..auth.. ..project2.. k/PARENTS-REL ..rt..) => []
-                       (core/get-entities ..auth.. [..file1.. ..file2.. ..folder1.. ..folder2.. ..project1.. ..project2..] ..rt..) => [{:_id        ..file1..
-                                                                                                                                        :type       k/FILE-TYPE
-                                                                                                                                        :attributes {:name ..filename1..}}
-                                                                                                                                       {:_id        ..file2..
-                                                                                                                                        :type       k/FILE-TYPE
-                                                                                                                                        :attributes {:name ..filename2..}}
-                                                                                                                                       {:_id        ..folder1..
-                                                                                                                                        :type       k/FOLDER-TYPE
-                                                                                                                                        :attributes {:name ..foldername1..}}
-                                                                                                                                       {:_id        ..folder2..
-                                                                                                                                        :type       k/FOLDER-TYPE
-                                                                                                                                        :attributes {:name ..foldername2..}}
-                                                                                                                                       {:_id        ..project1..
-                                                                                                                                        :type       k/PROJECT-TYPE
-                                                                                                                                        :attributes {:name ..projectname1..}}
-                                                                                                                                       {:_id        ..project2..
-                                                                                                                                        :type       k/PROJECT-TYPE
-                                                                                                                                        :attributes {:name ..projectname2..}}]]
+  (against-background [(b/get-parents ..auth.. ..file1.. ..rt..) => [{:_id ..folder1..} {:_id ..folder2..}]
+                       (b/get-parents ..auth.. ..file2.. ..rt..) => [{:_id ..folder2..}]
+                       (b/get-parents ..auth.. ..folder1.. ..rt..) => [{:_id ..project1..}]
+                       (b/get-parents ..auth.. ..folder2.. ..rt..) => [{:_id ..project1..} {:_id ..project2..}]
+                       (b/get-parents ..auth.. ..project1.. ..rt..) => []
+                       (b/get-parents ..auth.. ..project2.. ..rt..) => []
+                       (core/get-entities ..auth.. #{..file1.. ..folder1.. ..folder2.. ..file2.. ..project1.. ..project2..} ..rt..) => [{:_id        ..file1..
+                                                                                                                                         :type       k/FILE-TYPE
+                                                                                                                                         :attributes {:name ..filename1..}}
+                                                                                                                                        {:_id        ..file2..
+                                                                                                                                         :type       k/FILE-TYPE
+                                                                                                                                         :attributes {:name ..filename2..}}
+                                                                                                                                        {:_id        ..folder1..
+                                                                                                                                         :type       k/FOLDER-TYPE
+                                                                                                                                         :attributes {:name ..foldername1..}}
+                                                                                                                                        {:_id        ..folder2..
+                                                                                                                                         :type       k/FOLDER-TYPE
+                                                                                                                                         :attributes {:name ..foldername2..}}
+                                                                                                                                        {:_id        ..project1..
+                                                                                                                                         :type       k/PROJECT-TYPE
+                                                                                                                                         :attributes {:name ..projectname1..}}
+                                                                                                                                        {:_id        ..project2..
+                                                                                                                                         :type       k/PROJECT-TYPE
+                                                                                                                                         :attributes {:name ..projectname2..}}]]
 
     (fact "calculates file breadcrumbs"
       (b/get-breadcrumbs ..auth.. ..rt.. [..file1.. ..file2..]) => {..file1.. [[{:type k/FILE-TYPE :id ..file1.. :name ..filename1..}
