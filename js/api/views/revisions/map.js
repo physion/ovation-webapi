c1d7847b83ffbab45d00951735a998798350751e
Barry Wark <barrywark@gmail.com>
2016-10-03 22:03:25 -0400

    Updates revisions view

--- js/api/views/revisions/map.js
+++ js/api/views/revisions/map.js
@@ -1,14 +1,9 @@
 function (doc) {
-    if (doc.type && doc.type === "Revision") {
-        if (doc.attributes && doc.attributes.file_id && doc.attributes.previous) {
-            emit([doc.owner, doc.attributes.file_id], [[doc._id], doc.attributes.previous.length]);
-
-            if (doc.links && doc.links._collaboration_roots) {
-                var roots = doc.links._collaboration_roots;
-                for (var i = 0; i < roots.length; i++) {
-                    emit([roots[i], doc.attributes.file_id], [[doc._id], doc.attributes.previous.length]);
-                }
-            }
+    if(doc.type && doc.type === 'Revision') {
+        if(doc.attributes &&
+            doc.attributes.file_id &&
+            doc.attributes.previous) {
+            emit([doc.attributes.file_id, doc.attributes.previous.length], null);
         }
     }
 }
