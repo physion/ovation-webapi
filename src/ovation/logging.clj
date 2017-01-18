300c286cfe382e5a7b5e642405f4dc72b4e7d301
Barry Wark <barrywark@gmail.com>
2017-01-18 16:41:53 -0500

    Add logging and update config

--- src/ovation/logging.clj
+++ src/ovation/logging.clj
@@ -1,83 +1,14 @@
 (ns ovation.logging
   (:require [taoensso.timbre :as timbre]
-            ;[taoensso.timbre.appenders.3rd-party.logstash :as logstash-appender]
+            [taoensso.timbre.appenders.core :as appenders]
+            [taoensso.timbre.appenders.3rd-party.logstash :refer [logstash-appender]]
+            [taoensso.timbre.appenders.3rd-party.logstash :refer [logstash-appender]]
             [potemkin :refer [import-vars]]
-            [ovation.config :as config]
-            [cheshire.core :as cheshire])
-  (:import [java.net Socket InetAddress]
-           [java.io PrintWriter]))
-
+            [ovation.config :as config]))
 
 (import-vars
   [taoensso.timbre
    log debug info warn error fatal])
-
-
-;; copied from taoensso.timbre.appenders.3rd-party.logstash until it's in the JAR
-
-(defn connect
-  [host port]
-  (let [addr (InetAddress/getByName host)
-        sock (Socket. addr (Integer/parseInt port))]
-    [sock
-     (PrintWriter. (.getOutputStream sock))]))
-
-(defn connection-ok?
-  [[^Socket sock ^PrintWriter out]]
-  (and (not (.isClosed sock))
-    (.isConnected sock)
-    (not (.checkError out))))
-
-(def iso-format "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-
-(defn data->json-stream
-  [data writer opts]
-  ;; Note: this it meant to target the logstash-filter-json; especially "message" and "@timestamp" get a special meaning there.
-  (let [stacktrace-str (if-let [pr (:pr-stacktrace opts)]
-                         #(with-out-str (pr %))
-                         timbre/stacktrace)]
-    (cheshire/generate-stream
-      (merge (:context data)
-        {:level (:level data)
-         :namespace (:?ns-str data)
-         :file (:?file data)
-         :line (:?line data)
-         :stacktrace (some-> (force (:?err_ data)) (stacktrace-str))
-         :hostname (force (:hostname_ data))
-         :message (force (:msg_ data))
-         "@timestamp" (:instant data)})
-      writer
-      (merge {:date-format iso-format
-              :pretty false}
-        opts))))
-
-(defn logstash-appender
-  "Returns a Logstash appender, which will send each event in JSON
-  format to the logstash server at `host:port`. Additionally `opts`
-  may be a map with `:pr-stracktrace` mapped to a function taking an
-  exception, which should write the stacktrace of that exception to
-  `*out`."
-  [host port & [opts]]
-  (let [conn (atom nil)
-        nl "\n"]
-    {:enabled?   true
-     :async?     false
-     :min-level  nil
-     :rate-limit nil
-     :output-fn  :inherit
-     :fn
-                 (fn [data]
-                   (try
-                     (let [[sock out] (swap! conn
-                                        (fn [conn]
-                                          (or (and conn (connection-ok? conn) conn)
-                                            (connect host port))))]
-                       (locking sock
-                         (data->json-stream data out (select-keys opts [:pr-stacktrace]))
-                         ;; logstash tcp input plugin: "each event is assumed to be one line of text".
-                         (.write ^java.io.Writer out nl)))
-                     (catch java.io.IOException _
-                       nil)))}))
 
 
 (defn logging-config
@@ -85,10 +16,13 @@
   (if-let [host config/LOGGING_HOST]
     (let [port config/LOGGING_PORT]
       {:level     :info
+       :output-fn (partial timbre/default-output-fn {:stacktrace-fonts {}})
        :appenders {:timbre (logstash-appender host port)
-                   :println (timbre/println-appender {:stream :auto})}})
-    {:level     :error}))
-     ;:appenders {:println (timbre/println-appender {:stream :auto})}}))
+                   :println (appenders/println-appender {:stream :auto})}})
+
+    {:level     :info
+     :appenders {:println (appenders/println-appender {:stream :auto})
+                 :file    (appenders/spit-appender {:fname "/var/app/current/ovation.log"})}}))
 
 
 (defn setup! []
