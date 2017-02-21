0419ff0f3f32da613bd910b080408b8bf0593d9c
Barry Wark <barrywark@gmail.com>
2017-02-20 23:00:03 -0500

    Lein 2.4.3 it is

--- deploy_aws.sh
+++ deploy_aws.sh
@@ -2,6 +2,6 @@
 
 ENVIRONMENT=$1
 
-curl -L -O https://github.com/technomancy/leiningen/raw/2.6.1/bin/lein
+curl -L -O https://github.com/technomancy/leiningen/raw/2.4.3/bin/lein
 chmod u+x lein
 ./lein with-profile ci do clean, beanstalk deploy $ENVIRONMENT
