51714af3811ec4a4f22813104bb28d7948a45000
Barry Wark <barrywark@gmail.com>
2017-03-27 14:02:08 -0400

    Don't try sub

--- src/ovation/audit.clj
+++ src/ovation/audit.clj
@@ -1,13 +1,11 @@
 (ns ovation.audit
   (:require [ring.logger.messages :refer [request-details]]
             [ring.logger.protocols :refer [info]]
-            [clojure.tools.logging]
-            [ovation.auth :as auth]))
+            [clojure.tools.logging]))
 
 
 (defmethod request-details :identity-printer
   [{:keys [logger] :as options} req]
-  (info logger (str "[AUDIT] " (merge {:email (get-in req [:identity :email])
-                                       :identity (get-in req [:identity :sub])}
+  (info logger (str "[AUDIT] " (merge {:email (get-in req [:identity :email])}
                                 (select-keys req [:request-method
                                                   :uri])))))
