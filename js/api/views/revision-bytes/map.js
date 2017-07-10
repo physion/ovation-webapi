8c71ba706d0bd172b96fbbadcbbf15b686d98cea
Barry Wark <barrywark@gmail.com>
2017-07-10 02:39:23 -0400

    Add view/reduce function for safe keeping

---
+++ js/api/views/revision-bytes/map.js
@@ -0,0 +1,18 @@
+function (doc) {
+    var org = doc.organization;
+
+    if(doc.type && doc.type === 'Revision') {
+        if(!doc.trash_info) {
+            if(doc.attributes && doc.attributes.content_length) {
+                emit([org,doc.owner,doc._id], doc.attributes.content_length);
+
+                if (doc.links && doc.links._collaboration_roots) {
+                    var roots = doc.links._collaboration_roots;
+                    for (var i = 0; i < roots.length; i++) {
+                        emit([org, roots[i], doc.owner, doc._id], doc.attributes.content_length);
+                    }
+                }
+            }
+        }
+    }
+}
