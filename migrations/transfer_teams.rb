7de398a26b42cf2d5e3b40208cacd5658769a3ae
Barry Wark <barrywark@gmail.com>
2017-08-04 14:17:14 -0400

    Fix Raygun API handler

--- migrations/transfer_teams.rb
+++ migrations/transfer_teams.rb
@@ -1,10 +1,24 @@
-uuids = ["01047b3e-3fc1-4fab-9b3c-bf7f301ea8a5"]
-org_id = 247
+uuids = []
+
+puts "Organization id: "
+org_id = gets.chomp.to_i
 
 ActiveRecord::Base.transaction do
   teams = Team.where(uuid: uuids)
   teams.find_each do |team|
+    puts team.uuid
     team.entity_id = org_id
     team.save!
+
+    # Handle guest org memberships
+    team.memberships.each do |membership|
+      begin
+      membership.send(:ensure_organization_membership)
+      rescue
+        puts membership.user.email
+      end
+
+    end
   end
+
 end
