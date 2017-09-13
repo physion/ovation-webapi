e5dd64ee8f70253a5e278de3f1e932f0d7420fa8
Barry Wark <barrywark@gmail.com>
2017-09-13 13:05:14 -0400

    Remove vars

--- deploy/development/create-development-webapi-config.sh
+++ deploy/development/create-development-webapi-config.sh
@@ -5,13 +5,6 @@ set -e
 source dev.gke.env
 
 NAMESPACE=development
-KUBERNETES_CLUSTER_NAME=ovation
-KUBERNETES_APP_NAME=webapi
-DEFAULT_ZONE=us-east1-b
-PROJECT_ID=ovation-staging
-WORKDIR=deploy/development
-NAMESPACE=development
-
 
 kubectl create configmap development-webapi-config \
  --namespace=$NAMESPACE \
