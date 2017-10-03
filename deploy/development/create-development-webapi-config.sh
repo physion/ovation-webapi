c150ff2678fde66fb616a46ef65ee8c66a10ecfa
Barry Wark <barrywark@gmail.com>
2017-10-03 11:02:11 -0400

    Update config to include ELASTICSEARCH_URL

--- deploy/development/create-development-webapi-config.sh
+++ deploy/development/create-development-webapi-config.sh
@@ -10,4 +10,5 @@ kubectl create configmap development-webapi-config \
  --namespace=$NAMESPACE \
  --from-literal=CLOUDANT_DB_URL=$CLOUDANT_DB_URL \
  --from-literal=OVATION_IO_HOST_URI=$OVATION_IO_HOST_URI \
- --from-literal=GOOGLE_CLOUD_PROJECT_ID=$GOOGLE_CLOUD_PROJECT_ID
+ --from-literal=GOOGLE_CLOUD_PROJECT_ID=$GOOGLE_CLOUD_PROJECT_ID \
+ --from-literal=ELASTICSEARCH_URL=$ELASTICSEARCH_URL
