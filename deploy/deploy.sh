a1bc769a01820cfe23d1e7bb193802140507433e
Barry Wark <barrywark@gmail.com>
2017-10-12 22:06:43 -0400

    Echo deployment variables

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -29,18 +29,22 @@ helm init --upgrade
 echo "Installing helm-secrets"
 helm plugin install https://github.com/futuresimple/helm-secrets
 
-echo "Upgrading webapi relase..."
-# helm install…
-helm-wrapper install --dry-run --debug --namespace=$NAMESPACE --timeout 600 --wait \
-    --set image.tag=$NAMESPACE-$CI_TIMESTAMP \
-    --set ingress.staticIPAddressName=$NAMESPACE-webapi-static-ip \
-    -f ./deploy/values/$NAMESPACE/secrets.yaml \
-    $RELEASE_NAME \
+echo "Upgrading webapi release..."
+
+echo "NAMESPACE = $NAMESPACE"
+echo "RELEASE_NAME = $RELEASE_NAME"
+echo "CI_TIMESTAMP = $CI_TIMESTAMP"
+
+helm-wrapper install --dry-run --debug --namespace=${NAMESPACE} --timeout 600 --wait \
+    --set image.tag=${NAMESPACE}-${CI_TIMESTAMP} \
+    --set ingress.staticIPAddressName=${NAMESPACE}-webapi-static-ip \
+    -f ./deploy/values/${NAMESPACE}/secrets.yaml \
+    ${RELEASE_NAME} \
     ./deploy/ovation-webapi/
 
-helm-wrapper upgrade --install --namespace=$NAMESPACE --timeout 600 --wait \
-    --set image.tag=$NAMESPACE-$CI_TIMESTAMP \
-    --set ingress.staticIPAddressName=$NAMESPACE-webapi-static-ip \
-    -f ./deploy/values/$NAMESPACE/secrets.yaml \
-    $RELEASE_NAME \
+helm-wrapper upgrade --install --namespace=${NAMESPACE} --timeout 600 --wait \
+    --set image.tag=${NAMESPACE}-${CI_TIMESTAMP} \
+    --set ingress.staticIPAddressName=${NAMESPACE}-webapi-static-ip \
+    -f ./deploy/values/${NAMESPACE}/secrets.yaml \
+    ${RELEASE_NAME} \
     ./deploy/ovation-webapi/
