34f8ac84c2db0596fa2e44b6569ca9f5fe6deb89
Barry Wark <barrywark@gmail.com>
2018-03-01 11:07:09 -0500

    Skip elasticsearch deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -45,8 +45,6 @@ helm upgrade --install kube-lego-${NAMESPACE} stable/kube-lego\
     --set rbac.create=true
 
 # Update dependencies
-helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
-helm repo add cos https://centerforopenscience.github.io/helm-charts/
 helm dependencies update ./deploy/ovation-webapi/
 
 
